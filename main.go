4d034b60cc5ea7affe2db230b26d37eb6711ca50
Matt Peperell <matt@peperell.com>
2022-08-26 17:50:51 +0100

    Write only 1 log message per request

--- main.go
+++ main.go
@@ -17,10 +17,8 @@ func TagRequest(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	match := false
 	tag := params["tag"]
-	log.Print(fmt.Sprintf("Request for %s using %s from %s", tag, r.Proto, r.RemoteAddr))
 	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
 	if err != nil {
-		// URL file not found or can't be opened
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("URL file not found or can't be opened"))
 		log.Print("Received request but URL file was missing or could not be opened")
@@ -31,14 +29,14 @@ func TagRequest(w http.ResponseWriter, r *http.Request) {
 	for scanner.Scan() {
 		v := strings.Fields(scanner.Text())
 		if v[0] == tag {
-			log.Print(fmt.Sprintf("Match found for tag %s. Redirecting to %s", tag, v[1]))
+			log.Print(fmt.Sprintf("Request for %s (%s) using %s from %s", tag, v[1], r.Proto, r.RemoteAddr))
 			match = true
 			http.Redirect(w, r, v[1], http.StatusFound)
 			break
 		}
 	}
 	if match == false {
-		log.Print(fmt.Sprintf("No match found for tag %s", tag))
+		log.Print(fmt.Sprintf("Request for unknown tag %s using %s from %s", tag, r.Proto, r.RemoteAddr))
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("Tag not found"))
 	}
